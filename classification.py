afe587334638df0824954b9b52a27c69d968e647
Yujin Choi <rkadl7412hyh@naver.com>
2024-02-06 13:50:31 +0900

    2번 분류

--- classification.py
+++ classification.py
@@ -3,11 +3,14 @@ import shutil
 import tkinter
 from tkinter import filedialog, Label, Button, PhotoImage
 
-# 파일과 폴더 처리 기능
 def fileList(path_before: str) -> list:
     file_list = os.listdir(path_before)
-    category = [file.split("_")[1] for file in file_list if "_" in file]
-    return list(set(category))
+    categories = set()  # 중복을 방지하기 위해 set을 사용합니다.
+    for file in file_list:
+        if "_" in file:
+            category = file.split("_", 1)[1]  # 언더스코어를 기준으로 파일명을 두 부분으로 나누고, 두 번째 부분을 카테고리로 사용합니다.
+            categories.add(category)
+    return list(categories)
 
 def makeFolder(path_after: str, file_list: list):
     for file in file_list:
@@ -18,8 +21,27 @@ def makeFolder(path_after: str, file_list: list):
 
 def moveFile(path_before, path_after, file_list):
     for file in file_list:
-        category = file.split("_")[1] if "_" in file else "기타"
-        shutil.move(os.path.join(path_before, file), os.path.join(path_after, category))
+        # 파일이름에 '_'가 있는 경우에만 처리
+        if '_' in file:
+            filename, category = file.split('_', 1)  # '_'를 기준으로 최대 한 번만 나눕니다.
+            category_folder = os.path.join(path_after, category)
+            subcategory_folder = os.path.join(category_folder, filename.split('_')[0])
+
+            # 카테고리 폴더와 서브카테고리 폴더 생성
+            if not os.path.exists(category_folder):
+                os.makedirs(category_folder)
+            if not os.path.exists(subcategory_folder):
+                os.makedirs(subcategory_folder)
+            
+            # 파일 이동
+            shutil.move(os.path.join(path_before, file), subcategory_folder)
+        else:
+            # '_'가 없는 경우에도 카테고리 폴더 생성 후 이동
+            category_folder = os.path.join(path_after, file.split('_')[0])
+            if not os.path.exists(category_folder):
+                os.makedirs(category_folder)
+            shutil.move(os.path.join(path_before, file), category_folder)
+
 
 # tkinter GUI
 def select_source_folder():
@@ -62,7 +84,7 @@ tkinter.Button(window, text="분류 시작", command=classify_files).pack()
 
 gif_label = Label(window)
 gif_label.pack()
-photo = PhotoImage(file="/Users/dhl/Desktop/ufo-ezgif.com-video-to-gif-converter.gif")
-gif_label.config(image=photo)
+# photo = PhotoImage(file="/Users/dhl/Desktop/ufo-ezgif.com-video-to-gif-converter.gif")
+# gif_label.config(image=photo)
 
 window.mainloop()
